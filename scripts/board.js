10e1d0e14b3fef7aad86dbeaf9fa27e322cbaf68
Rick Lamoreaux <rlamorea@gmail.com>
2024-07-21 20:20:48 -0700

    fixes #4, fixes #5, fixes #6, fixes #7, fixes #8 - refactor and back to baseline

--- scripts/board.js
+++ scripts/board.js
@@ -3,99 +3,52 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 const BOARD_WIDTH = 52
+const CHIP_ROWS = 1
 const PIN_ROWS = 5
-const POWER_CONNECTION_METHODS = [ 'none', 'top-rails', 'first-rails' ]
-const GRID_X_OFFSET = 12
-const GRID_Y_OFFSET = 15
-const GRID_SIZE = 16
+const POWER_CONNECTION_METHODS = [ 'none', 'default-power', 'split-power-top', 'split-ground-top' ]
 
 class Board {
   constructor(div) {
     this.div = div
-
-    this.addRail(0)
-    this.addRail(1)
-    this.addGap('numbers')
-    this.addPins('top')
-    this.addGap('wide')
-    this.addPins('bottom')
-    this.addGap('numbers')
-    this.addRail(2)
-    this.addRail(3)
-
-    this.colHighlight = document.createElement('div')
-    this.colHighlight.classList.add('highlight', 'col-highlight')
-    this.div.appendChild(this.colHighlight)
-    this.colHighlight.style.display = 'none'
-
-    this.rowHighlight = document.createElement('div')
-    this.rowHighlight.classList.add('highlight', 'row-highlight')
-    this.div.appendChild(this.rowHighlight)
-    this.rowHighlight.style.display = 'none'
-
-    this.connectHighlight = document.createElement('div')
-    this.connectHighlight.classList.add('highlight', 'connect-highlight')
-    this.div.appendChild(this.connectHighlight)
-    this.connectHighlight.style.display = 'none'
-
-    this.power = document.createElement('div')
-    this.power.classList.add('power', 'fivevolts')
-    this.power.innerHTML = '+5V'
-    this.div.appendChild(this.power)
-    this.power.addEventListener('click', () => { this.cyclePowerWireup() })
-    this.powerConnectionMethod = 'none'
-
-    this.ground = document.createElement('div')
-    this.ground.classList.add('power', 'ground')
-    this.ground.innerHTML = 'Gnd'
-    this.div.appendChild(this.ground)
-
-    this.railEnds = {}
-    for (let r = 0; r < 4; r++) {
-      this.railEnds[`r${r}-left`] = this.div.querySelector(`.plug[data-ref="rail"][data-index="${r}"][data-col="0"]`)
-      this.railEnds[`r${r}-right`] = this.div.querySelector(`.plug[data-ref="rail"][data-index="${r}"][data-col="${BOARD_WIDTH-1}"]`)
+    this.dimensions = {
+      width: BOARD_WIDTH,
+      chips: CHIP_ROWS,
+      pins: PIN_ROWS
     }
-    this.wireLayers = {}
-    this.wireId = 0
-    this.wireGrid = []
-    const { x: endX, y : endY } = this.gridLock(this.ground)
-    for (let r = 0; r <= endY; r++) {
-      let row = []
-      for (let c = 0; c <= endX; c++) {
-        row.push([])
+
+    this.rails = []
+    this.chipRails = []
+
+    this.addRowRails(2)
+    for (let chipRow = 0; chipRow < CHIP_ROWS; chipRow++) {
+      this.chipRails.push({ top: [], bottom: [] })
+      this.addGap('numbers')
+      this.addPinRails('top', chipRow)
+      this.addGap('chip')
+      this.addPinRails('bottom', chipRow)
+      this.addGap('numbers')
+      if (chipRow + 1 < CHIP_ROWS) {
+        this.addRowRails(2)
       }
-      this.wireGrid.push(row)
     }
+    this.addRowRails(2)
+
+    this.columnHighlight = this.createHighlight('col-highlight')
+    this.rowHighlight = this.createHighlight('row-highlight')
+    this.railHighlight = this.createHighlight('rail-highlight')
+
+    this.powerNode = this.createNode('fivevolts', '+5V')
+    this.groundNode = this.createNode('ground', 'GND')
+    this.powerConnectionMethod = 'none'
+    this.powerWires = []
+
+    Wire.initialize(this)
   }
 
   addLabel(div, label) {
     const span = document.createElement('span')
     if (label) { span.innerHTML = label }
     div.appendChild(span)
-  }
-
-  addPlug(div, ref, index, extra = {}) {
-    const plug = document.createElement('span')
-    plug.classList.add('plug')
-    plug.dataset.ref = ref
-    plug.dataset.index = index.toString()
-    for (const key in extra) {
-      plug.dataset[key] = extra[key]
-    }
-    plug.addEventListener('mouseenter', () => { this.moveHighlightTo(plug) })
-    plug.addEventListener('mouseleave', () => { this.hideHighlight() })
-    div.appendChild(plug)
-  }
-
-  addRail(railIndex) {
-    const row = document.createElement('div')
-    row.classList.add('plugrow')
-    this.addLabel(row)
-    for (let w = 0; w < BOARD_WIDTH; w++) {
-      this.addPlug(row, 'rail', railIndex, { col: w })
-    }
-    this.addLabel(row)
-    this.div.appendChild(row)
   }
 
   addGap(variant) {
@@ -107,150 +60,124 @@ class Board {
         this.addLabel(gap, (w === 0 || (w % 5) === 4) ? w+1 : null)
       }
       this.addLabel(gap)
-    } else if (variant === 'wide') {
-      gap.classList.add('wide')
+    } else if (variant === 'chip') {
+      gap.classList.add('chip')
     }
     this.div.appendChild(gap)
   }
 
-  addPins(location) {
-    for (let r = 0; r < PIN_ROWS; r++) {
-      const row = document.createElement('div')
-      row.classList.add('plugrow')
-      this.addLabel(row, String.fromCharCode(65 + r))
-      for (let w = 0; w < BOARD_WIDTH; w++) {
-        this.addPlug(row, location, w, { row: r })
-      }
-      this.addLabel(row, String.fromCharCode(65 + r))
-      this.div.appendChild(row)
+  addRow(parent) {
+    if (!parent) { parent = this.div }
+    const row = document.createElement('div')
+    row.classList.add('plugrow')
+    parent.appendChild(row)
+    return row
+  }
+
+  addRowRails(count) {
+    for (let row = 0; row < count; row++) {
+      const row = this.addRow()
+      this.rails.push(new Rail(this, row, 'rail', this.rails.length))
     }
   }
 
-  moveHighlightTo(plug) {
-    this.colHighlight.style.display = 'block'
-    this.colHighlight.style.left = `${plug.offsetLeft}px`
+  addPinRails(location, index) {
+    const pinBlock = document.createElement('div')
+    pinBlock.classList.add('pin-block')
+    this.div.appendChild(pinBlock)
+    let rows = []
+    for (let rowIndex = 0; rowIndex < PIN_ROWS; rowIndex++) {
+      const row = this.addRow(pinBlock)
+      this.addLabel(row, String.fromCharCode(65 + rowIndex))
+      rows.push(row)
+    }
+    for (let col = 0; col < BOARD_WIDTH; col++) {
+      this.chipRails[index][location] = new Rail(this, pinBlock, `pin-${location}`, index)
+    }
+    for (let row = 0; row < PIN_ROWS; row++) {
+      this.addLabel(rows[row], String.fromCharCode(65 + row))
+    }
+  }
+
+  createHighlight(type) {
+    const highlight = document.createElement('div')
+    highlight.classList.add('highlight', type)
+    this.div.appendChild(highlight)
+    highlight.style.display = 'none'
+    return highlight
+  }
+
+  moveGridHighlightTo(plug) {
+    this.columnHighlight.style.display = 'block'
+    this.columnHighlight.style.left = `${plug.offsetLeft}px`
     this.rowHighlight.style.display = 'block'
     this.rowHighlight.style.top = `${plug.offsetTop}px`
+  }
 
-    const plugType = plug.dataset.ref
-    if (plugType === 'rail') {
-      const railStart = plug.parentElement.querySelector('.plug:nth-child(2)')
-      const railEnd = this.div.querySelector(`.plug:nth-child(${BOARD_WIDTH+1})`)
-      this.connectHighlight.style.display = 'block'
-      this.connectHighlight.style.left = `${railStart.offsetLeft-1}px`
-      this.connectHighlight.style.top = `${railStart.offsetTop-1}px`
-      this.connectHighlight.style.width = `${(railEnd.offsetLeft + railEnd.offsetWidth) - railStart.offsetLeft}px`
-      this.connectHighlight.style.height = `${railStart.offsetHeight}px`
-    } else {
-      let topRow = plug.parentElement
-      while (1 === 1) {
-        if (topRow.previousElementSibling.classList.contains('gap')) { break }
-        topRow = topRow.previousElementSibling
-      }
-      let bottomRow = plug.parentElement
-      while (1 === 1) {
-        if (bottomRow.nextElementSibling.classList.contains('gap')) { break }
-        bottomRow = bottomRow.nextElementSibling
-      }
-      const pinStart = topRow.querySelector(`.plug[data-index="${plug.dataset.index}"]`)
-      const pinEnd = bottomRow.querySelector(`.plug[data-index="${plug.dataset.index}"]`)
-      this.connectHighlight.style.display = 'block'
-      this.connectHighlight.style.left = `${pinStart.offsetLeft-1}px`
-      this.connectHighlight.style.top = `${pinStart.offsetTop-1}px`
-      this.connectHighlight.style.width = `${pinStart.offsetWidth}px`
-      this.connectHighlight.style.height =   `${(pinEnd.offsetTop + pinEnd.offsetHeight) - pinStart.offsetTop}px`
-    }
+  moveRailHighlightTo(firstPlug, lastPlug) {
+    this.railHighlight.style.display = 'block'
+    this.railHighlight.style.left = `${firstPlug.offsetLeft-1}px`
+    this.railHighlight.style.top = `${firstPlug.offsetTop-1}px`
+    this.railHighlight.style.width = `${(lastPlug.offsetLeft + lastPlug.offsetWidth) - firstPlug.offsetLeft}px`
+    this.railHighlight.style.height =  `${(lastPlug.offsetTop + lastPlug.offsetHeight) - firstPlug.offsetTop}px`
   }
 
   hideHighlight() {
-    this.colHighlight.style.display = 'none'
+    this.columnHighlight.style.display = 'none'
     this.rowHighlight.style.display = 'none'
-    this.connectHighlight.style.display = 'none'
+    this.railHighlight.style.display = 'none'
   }
 
-  addWireLayer(layerName) {
-    if (!(layerName in this.wireLayers)) {
-      const layer = document.createElement('div')
-      layer.classList.add('wirelayer')
-      this.wireLayers[layerName] = { div: layer, wires: [] }
-      this.div.appendChild(layer)
-    }
-    return this.wireLayers[layerName]
+  createNode(type, text) {
+    const node = document.createElement('div')
+    node.classList.add('power', type)
+    node.innerHTML = text
+    node.addEventListener('click', e => { this.cyclePowerConnection() })
+    this.div.appendChild(node)
+    return node
   }
 
-  cyclePowerWireup() {
+  cyclePowerConnection() {
     let powerIdx = (POWER_CONNECTION_METHODS.indexOf(this.powerConnectionMethod) + 1) % POWER_CONNECTION_METHODS.length
     this.powerConnectionMethod = POWER_CONNECTION_METHODS[powerIdx]
 
-    const { div, wires } = this.addWireLayer('power')
-    div.innerHTML = ''
-    // TODO: clear power wire IDs from grid
-    // TODO: clear power from rail and plugs
-    // TODO: make plugs unused
-
-    let checkPlugs = {}
-    switch (this.powerConnectionMethod) {
-      case 'top-rails':
-        checkPlugs = { 'r0-left': 'power', 'r1-left': 'power', 'r2-right': 'ground', 'r3-right': 'ground' }
-        break
-      case 'first-rails':
-        checkPlugs = { 'r0-left': 'power', 'r1-right': 'ground', 'r2-left': 'power', 'r3-right': 'ground' }
-        break
-      case 'none':
-      default:
+    // remove existing wires
+    for (const wire of this.powerWires) {
+      wire.eraseWire()
     }
+    this.powerWires = []
 
-    let inuse = false
-    for (const checkPlug in checkPlugs) {
-      const plug = this.railEnds[checkPlug]
-      if (!plug.dataset.hardWired) { inuse = true; break; }
+    for (let railIndex = 0; railIndex < this.rails.length; railIndex++) {
+      let fromNode = null
+      let power = ''
+      let conMethod = this.powerConnectionMethod
+      switch (this.powerConnectionMethod) {
+        case 'default-power':
+          if (railIndex < 2) {
+            fromNode = this.powerNode
+            power = 'power'
+            break
+          } else if (railIndex >= this.rails.length - 2) {
+            fromNode = this.groundNode
+            power = 'ground'
+            break
+          }
+          conMethod = 'split-ground-top' // for middle rails, ground on top
+        case 'split-power-top':
+        case 'split-ground-top':
+          let topPower = conMethod.split('-')[1]
+          power = (railIndex % 2 === 0) ? topPower : 'opposite'
+          if (power === 'opposite') { power = topPower === 'power' ? 'ground' : 'power' }
+          fromNode = (power === 'power') ? this.powerNode : this.groundNode
+        case 'none':
+        default:
+          break
+      }
+      if (fromNode) {
+        const rail = this.rails[railIndex]
+        const toNode = (power === 'power') ? rail.powerPlug : rail.groundPlug
+        this.powerWires.push(new Wire(fromNode, toNode, { power }))
+      }
     }
-    if (inuse) {
-      this.powerConnectionMethod = 'none'
-      return // leave unconnected
-    }
-    // if all the 
-  }
-
-  gridLock(div) {
-    let x = div.offsetLeft + (div.offsetWidth / 2)
-    let y = div.offsetTop + (div.offsetHeight / 2)
-    return { x, y }
-  }
-
-  gridRestore(x, y) {
-    x = Math.floor( ((x - GRID_X_OFFSET) / GRID_SIZE ))
-    y = Math.floor( ((y - GRID_Y_OFFSET) / GRID_SIZE ))
-    x = x * GRID_SIZE + GRID_SIZE / 2 + GRID_X_OFFSET
-    y = y * GRID_SIZE + GRID_SIZE / 2 + GRID_Y_OFFSET
-    return { x, y }
-  }
-
-  drawWire(layerDiv, source, dest, color = null, connectedClass = null) {
-    const {x: sourceX, y: sourceY} = this.gridLock(source)
-    const {x: destX, y: destY} = this.gridLock(dest)
-
-    if (sourceY === destY && sourceX === destX) { // no line
-      return // nothing to draw
-    } else if (sourceX === destX) {
-
-    }
-
-    let svg = layerDiv.querySelector('svg')
-    if (!svg) {
-      svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-      svg.setAttribute("aria-hidden","true")
-      svg.setAttribute('viewbox', `0 0 ${layerDiv.offsetWidth} ${layerDiv.offsetHeight}`)
-      svg.setAttribute('width', `${layerDiv.offsetWidth}px`)
-      svg.setAttribute('height', `${layerDiv.offsetHeight}px`)
-      layerDiv.appendChild(svg)
-    }
-    const wirePath = document.createElementNS("http://www.w3.org/2000/svg", 'path')
-    wirePath.setAttribute('d', `M${sourceX} ${sourceY} L ${destX} ${destY}`)
-    wirePath.setAttribute('stroke', color)
-    wirePath.setAttribute('stroke-width', '3px')
-    wirePath.setAttribute('fill', 'none')
-
-    svg.appendChild(wirePath);
   }
 }
---
+++ scripts/wire.js
@@ -0,0 +1,135 @@
+class Wire {
+  constructor(from, to, options) {
+    this.wireId = Wire.wireId++
+    this.from = from
+    this.fromElem = (from instanceof Plug) ? from.span : from
+    this.to = to
+    this.toElem = (to instanceof Plug) ? to.span : to
+    this.color = options.color || Wire.nextColor()
+    this.power = options.power
+
+    if (this.power) {
+      this.color = options.power === 'power' ? 'red' : 'black'
+      const { left: endLeft, top: endTop } = Wire.gridAlign({ left: this.toElem.offsetLeft, top: this.toElem.offsetTop })
+      let path = [ Wire.powerGrid[this.power].coordinates ]
+      if (Wire.powerGrid[this.power].coordinates.top !== endTop) {
+        path.push({ left: Wire.powerGrid[this.power].channelLeft, top: Wire.powerGrid[this.power].coordinates.top })
+        path.push({ left: Wire.powerGrid[this.power].channelLeft, top: endTop })
+      }
+      path.push({ left: endLeft, top: endTop })
+      this.drawWire(path)
+      if (this.to instanceof Plug) { this.to.setStatus(this.power) }
+    } else {
+
+    }
+  }
+
+  drawWire(path) {
+    let wirePath = document.createElementNS("http://www.w3.org/2000/svg", 'path')
+    wirePath.id = `wire-${this.wireId}`
+    wirePath.setAttribute('stroke', this.color)
+    wirePath.setAttribute('stroke-width', '3px')
+    wirePath.setAttribute('stroke-linecap', `round`)
+    wirePath.setAttribute('fill', 'none')
+
+    let command = 'M'
+    let pathStr = ''
+    for (const point of path) {
+      pathStr += `${command} ${point.left} ${point.top} `
+      command = 'L'
+    }
+    wirePath.setAttribute('d', pathStr.trim())
+    Wire.svg.appendChild(wirePath)
+  }
+
+  eraseWire() {
+    const wirePath = Wire.svg.querySelector(`#wire-${this.wireId}`)
+    wirePath.remove()
+    if (this.to instanceof Plug) { this.to.setStatus('open') }
+  }
+}
+
+Wire.wireId = 1
+Wire.wireColorIndex = 0
+Wire.wireColors = [
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'cyan',
+  'purple'
+]
+
+Wire.nextColor = () => {
+  const col = Wire.wireColors[Wire.wireColorIndex]
+  Wire.wireColorIndex = (Wire.wireColorIndex + 1) % Wire.wireColors.length
+  return col
+}
+
+Wire.gridCoordinates = (point) => {
+  const left = Math.floor((point.left - Wire.gridOffset.left + Wire.cellSize / 2) / Wire.cellSize )
+  const top = Math.floor( (point.top - Wire.gridOffset.top + Wire.cellSize / 2) / Wire.cellSize )
+  return { column: left, row: top }
+}
+
+Wire.getOffset = (gridCoordinates) => {
+  return {
+    left: gridCoordinates.column * Wire.cellSize + Wire.gridOffset.left,
+    top: gridCoordinates.row * Wire.cellSize + Wire.gridOffset.top
+  }
+}
+
+Wire.gridAlign = (point) => {
+  return Wire.getOffset(Wire.gridCoordinates(point))
+}
+
+Wire.initialize = (board) => {
+  // build the grid
+  const plugs = Array.from(board.div.querySelectorAll('.plug'))
+  const firstPlug = plugs.find((e) => { return !e.classList.contains('powered') } )
+  const lastPlug = plugs.findLast((e) => { return !e.classList.contains('grounded') } )
+  Wire.cellSize = Math.round((lastPlug.offsetLeft - firstPlug.offsetLeft) / board.dimensions.width)
+  Wire.gridOffset = {
+    left: firstPlug.offsetLeft + Math.floor(Wire.cellSize / 2) - 2,
+    top: firstPlug.offsetTop + Math.floor (Wire.cellSize / 2) - 2
+  }
+  const gridRows = Math.round((lastPlug.offsetTop + lastPlug.offsetHeight - firstPlug.offsetTop) / Wire.cellSize)
+  Wire.grid = Array.from(
+    { length: gridRows },
+    (e) => Array.from({ length: board.dimensions.width }, e => 'empty' )
+  )
+
+  Wire.powerGrid = {
+    power: {
+      coordinates: Wire.gridAlign({
+        left: board.powerNode.offsetLeft + board.powerNode.offsetWidth / 2,
+        top: board.powerNode.offsetTop + board.powerNode.offsetHeight / 2,
+      })
+    },
+    ground: {
+      coordinates: Wire.gridAlign({
+        left: board.groundNode.offsetLeft + board.groundNode.offsetWidth / 2,
+        top: board.groundNode.offsetTop + board.groundNode.offsetHeight / 2,
+      })
+    }
+  }
+  Wire.powerGrid.power.channelLeft = Wire.gridAlign({
+    left: (Wire.powerGrid.power.coordinates.left + (Wire.getOffset({column: -1, row: 0}).left - Wire.powerGrid.power.coordinates.left) / 2),
+    top: 0
+  }).left
+  Wire.powerGrid.ground.channelLeft = Wire.gridAlign({
+    left: (Wire.powerGrid.ground.coordinates.left - (Wire.powerGrid.ground.coordinates.left - Wire.getOffset({column: board.dimensions.width + 1, row: 0}).left) / 2),
+    top: 0
+  }).left
+
+  const wireLayer = document.createElement("div")
+  wireLayer.classList.add('wirelayer')
+  board.div.appendChild(wireLayer)
+
+  Wire.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+  Wire.svg.setAttribute("aria-hidden","true")
+  Wire.svg.setAttribute('viewbox', `0 0 ${wireLayer.offsetWidth} ${wireLayer.offsetHeight}`)
+  Wire.svg.setAttribute('width', `${wireLayer.offsetWidth}px`)
+  Wire.svg.setAttribute('height', `${wireLayer.offsetHeight}px`)
+  wireLayer.appendChild(Wire.svg)
+}
